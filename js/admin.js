63c5f3bfdc5e0a2184039a64dec43ea440c69a1c
Guilherme Cardoso <80872320+CardosoGuiVi@users.noreply.github.com>
2024-12-28 03:06:58 -0300

    v2?

--- js/admin.js
+++ js/admin.js
@@ -2,11 +2,24 @@ import { db } from "./firebase-config.js";
 import { ref, set } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
 document.getElementById('blackout').addEventListener('click', () => {
-    set(ref(db, 'settings'), { animation: "blackout", color1: "#000000", color2: "#000000" });
+    set(ref(db, 'settings'), {
+        animation: "blackout",
+        color1: "#000000",
+        color2: "#000000"
+    });
 });
 
 document.getElementById('logo').addEventListener('click', () => {
-    set(ref(db, 'settings'), { animation: "logo", message: "Tudo certo! Aguarde nosso culto começar…" });
+    set(ref(db, 'settings'), {
+        animation: "logo",
+        message: "Tudo certo! Aguarde nosso culto começar…",
+        color1: "#000000",
+        color2: "#000000"
+    });
+});
+
+document.getElementById('blink-time').addEventListener('input', () => {
+    document.getElementById('blink-time-text').innerText = `Blink time (seg): ${document.getElementById('blink-time').value}`;
 });
 
 document.getElementById('blink').addEventListener('click', () => {
@@ -14,5 +27,10 @@ document.getElementById('blink').addEventListener('click', () => {
     const color2 = document.getElementById('color2').value;
     const blinkTime = document.getElementById('blink-time').value;
 
-    set(ref(db, 'settings'), { animation: "blink", color1, color2, blinkTime });
+    set(ref(db, 'settings'), {
+        animation: "blink",
+        color1,
+        color2,
+        blinkTime
+    });
 });
